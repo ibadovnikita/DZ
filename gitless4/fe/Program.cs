ee670fc98c3c7d6a328240115068d91ff0462cf8
IbadovNikita <blackcronos362@gmail.com>
2023-03-19 02:54:51 +0300

    задача 21

---
+++ gitless4/fe/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter x1 ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter y1 ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter z1 ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter x2 ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter y2 ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter z2 ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+double result = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
+Console.Write(result);
