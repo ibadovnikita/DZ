637c4cac9106b111611cf0c1f7d5887846cfd0a5
IbadovNikita <blackcronos362@gmail.com>
2023-03-19 03:27:19 +0300

    Задача 23

--- Program.cs
+++ Program.cs
@@ -1,14 +1,7 @@
-﻿Console.Write("Enter x1 ");
-double x1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Enter y1 ");
-double y1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Enter z1 ");
-double z1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Enter x2 ");
-double x2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Enter y2 ");
-double y2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Enter z2 ");
-double z2 = Convert.ToDouble(Console.ReadLine());
-double result = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
-Console.Write(result);
+﻿Console.Write("Enter N ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i < N+1; i++)
+{
+   Console.Write( Math.Pow(i,3) );
+   Console.Write(" ");
+}
